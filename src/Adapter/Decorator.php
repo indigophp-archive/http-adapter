642698d16d4f21debbb8ebe661f2333d4d3134e2
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-10-17 01:27:48 +0200

    Adds a generic Adapter Decorator

---
+++ src/Adapter/Decorator.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Indigo HTTP Adapter package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Indigo\Http\Adapter;
+
+use Indigo\Http\Adapter;
+
+/**
+ * Generic Adapter decorator implementation
+ *
+ * Should be used with an Adapter
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+trait Decorator
+{
+    /**
+     * @var Adapter
+     */
+    private $adapter;
+
+    /**
+     * @param Adapter $adapter
+     */
+    public function __construct(Adapter $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
+    /**
+     * Returns the decorated adapter
+     *
+     * @return Adapter
+     */
+    public function getAdapter()
+    {
+        return $this->adapter;
+    }
+}
