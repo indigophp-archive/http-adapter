2de58f06f5c53758ccc1de48f5220f1378d88070
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-10-19 16:48:08 +0200

    Adds Guzzle3 adapter

---
+++ spec/Indigo/Http/Adapter/Guzzle3Spec.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace spec\Indigo\Http\Adapter;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Guzzle\Http\ClientInterface;
+use Guzzle\Http\Message\RequestInterface as GuzzleRequest;
+use Guzzle\Http\Message\Response as GuzzleResponse;
+use PhpSpec\ObjectBehavior;
+
+class Guzzle3Spec extends ObjectBehavior
+{
+    protected $client;
+
+    function let(ClientInterface $client)
+    {
+        $this->beConstructedWith($client);
+        $this->client = $client;
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Indigo\Http\Adapter\Guzzle3');
+        $this->shouldHaveType('Indigo\Http\Adapter');
+    }
+
+    function it_should_return_a_response(Request $request, GuzzleRequest $guzzleRequest, GuzzleResponse $guzzleResponse)
+    {
+        $request->getMethod()->willReturn('GET');
+        $request->getUrl()->willReturn('http://foo.com');
+        $request->getProtocolVersion()->willReturn('1.1');
+        $request->getHeaders()->willReturn([]);
+        $request->getBody()->willReturn(null);
+
+        $guzzleResponse->getStatusCode()->willReturn(200);
+        $guzzleResponse->getProtocolVersion()->willReturn('1.1');
+        $guzzleResponse->getHeaders()->willReturn([]);
+        $guzzleResponse->getBody()->willReturn(null);
+
+        $this->client->createRequest('GET', 'http://foo.com', ['version' => '1.1', 'headers' => []])->willReturn($guzzleRequest);
+        $this->client->send($guzzleRequest)->willReturn($guzzleResponse);
+
+        $response = $this->send($request);
+
+        $response->getStatusCode()->shouldBe(200);
+        $response->getProtocolVersion()->shouldBe('1.1');
+    }
+}
---
+++ src/Adapter/Guzzle3.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+ * This file is part of the Indigo HTTP Adapter package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Indigo\Http\Adapter;
+
+use Indigo\Http\Adapter;
+use Indigo\Http\Stream;
+use Indigo\Http\Response;
+use Indigo\Http\Exception\RequestException;
+use Psr\Http\Message\RequestInterface as Request;
+use Guzzle\Http\ClientInterface;
+use Guzzle\Http\Exception\RequestException as GuzzleRequestException;
+
+/**
+ * Guzzle 3 Adapter
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+class Guzzle3 implements Adapter
+{
+    /**
+     * @var ClientInterface
+     */
+    private $client;
+
+    /**
+     * @param ClientInterface $client
+     */
+    public function __construct(ClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function send(Request $request)
+    {
+        $request = $this->transformRequest($request);
+
+        try {
+            $response = $this->client->send($request);
+        } catch (GuzzleRequestException $e) {
+            throw new RequestException($e->getMessage(), $e->getCode(), $e);
+        }
+
+        return $this->transformResponse($response);
+    }
+
+    /**
+     * Creates a Guzzle Request
+     *
+     * @param Request $request
+     *
+     * @return Guzzle\Http\Message\RequestInterface
+     */
+    private function transformRequest(Request $request)
+    {
+        $method = $request->getMethod();
+        $url = $request->getUrl();
+        $options = [
+            'version' => $request->getProtocolVersion(),
+            'headers' => $request->getHeaders(),
+        ];
+
+        if ($body = $request->getBody()) {
+            $options['body'] = $body->detach();
+        }
+
+        return $this->client->createRequest($method, $url, $options);
+    }
+
+    /**
+     * Returns a Response
+     *
+     * @param Guzzle\Http\Message\Response $response
+     *
+     * @return Response
+     */
+    private function transformResponse($response)
+    {
+        $transformed = new Response;
+
+        $transformed->setStatusCode($response->getStatusCode());
+
+        foreach ($response->getHeaders() as $header => $value) {
+            $transformed->setHeader($header, $value);
+        }
+
+        if ($body = $response->getBody()) {
+            $body = new Stream($body->detach());
+            $transformed->setBody($body);
+        }
+
+        $transformed->setProtocolVersion($response->getProtocolVersion());
+
+        return $transformed;
+    }
+}
