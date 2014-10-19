151f09b3aef18aaf7bc3f47d032265ac811fe2dc
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-10-19 20:59:33 +0200

    Removes spagetthi

--- src/Adapter/Cache.php
+++ src/Adapter/Cache.php
@@ -34,8 +34,7 @@ class Cache implements Adapter, PoolInterface
     public function send(Request $request)
     {
         // We hash it because / is not enabled in Stash
-        $hash = $request->getUrl();
-        $hash = md5(strval($hash));
+        $hash = md5($request->getUrl());
 
         $item = $this->getItem($hash);
         $cached = $item->get();
