078bfab1d11916c8ce369b9b009b2526a4b7a8ec
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-11-21 02:10:57 +0100

    Adds Client

---
+++ spec/Indigo/Http/ClientSpec.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace spec\Indigo\Http;
+
+use Indigo\Http\Adapter;
+use Psr\Http\Message\OutgoingRequestInterface as Request;
+use Psr\Http\Message\IncomingResponseInterface as Response;
+use Psr\Http\Message\StreamableInterface as Stream;
+use PhpSpec\ObjectBehavior;
+
+class ClientSpec extends ObjectBehavior
+{
+    function let(Adapter $adapter, Request $request, Response $response, Stream $stream)
+    {
+        $adapter->send($request)->willReturn($response);
+        $response->getBody()->willReturn($stream);
+        $stream->getContents()->willReturn('content');
+
+        $this->beConstructedWith($adapter);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Indigo\Http\Client');
+    }
+
+    function it_should_be_able_to_send_request(Request $request)
+    {
+        $this->send($request)->shouldReturn('content');
+    }
+
+    function it_should_be_able_to_create_request()
+    {
+        $request = $this->createRequest('GET', 'http://google.hu', [
+            'protocol_version' => '1.0',
+            'body' => 'content',
+            'headers' => [
+                'X-Test' => '1'
+            ],
+        ]);
+
+        $request->getMethod()->shouldReturn('GET');
+        $request->getUrl()->shouldReturn('http://google.hu');
+        $request->getProtocolVersion()->shouldReturn('1.0');
+        $request->getBody()->shouldImplement('Psr\Http\Message\StreamableInterface');
+        $request->getHeader('X-Test')->shouldReturn('1');
+    }
+
+    function it_should_be_able_to_send_get_request(Adapter $adapter, Response $response)
+    {
+        $request = $this->createRequest('GET', null, []);
+        $adapter->send($request)->willReturn($response);
+
+        $this->get()->shouldReturn('content');
+    }
+
+    function it_should_be_able_to_send_post_request(Adapter $adapter, Response $response)
+    {
+        $request = $this->createRequest('POST', null, []);
+        $adapter->send($request)->willReturn($response);
+
+        $this->post()->shouldReturn('content');
+    }
+
+    function it_should_be_able_to_send_put_request(Adapter $adapter, Response $response)
+    {
+        $request = $this->createRequest('PUT', null, []);
+        $adapter->send($request)->willReturn($response);
+
+        $this->put()->shouldReturn('content');
+    }
+
+    function it_should_be_able_to_send_head_request(Adapter $adapter, Response $response)
+    {
+        $request = $this->createRequest('HEAD', null, []);
+        $adapter->send($request)->willReturn($response);
+
+        $this->head()->shouldReturn('content');
+    }
+
+    function it_should_be_able_to_send_delete_request(Adapter $adapter, Response $response)
+    {
+        $request = $this->createRequest('DELETE', null, []);
+        $adapter->send($request)->willReturn($response);
+
+        $this->delete()->shouldReturn('content');
+    }
+
+    function it_should_be_able_to_send_patch_request(Adapter $adapter, Response $response)
+    {
+        $request = $this->createRequest('PATCH', null, []);
+        $adapter->send($request)->willReturn($response);
+
+        $this->patch()->shouldReturn('content');
+    }
+
+    function it_should_be_able_to_send_options_request(Adapter $adapter, Response $response)
+    {
+        $request = $this->createRequest('OPTIONS', null, []);
+        $adapter->send($request)->willReturn($response);
+
+        $this->options()->shouldReturn('content');
+    }
+}
---
+++ src/Client.php
@@ -0,0 +1,176 @@
+<?php
+
+/*
+ * This file is part of the Indigo HTTP Adapter package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Indigo\Http;
+
+use Psr\Http\Message\OutgoingRequestInterface as Request;
+
+/**
+ * Implementation of generic HTTP Client
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+class Client
+{
+    /**
+     * HTTP Client Adapter
+     *
+     * @var Adapter
+     */
+    protected $adapter;
+
+    /**
+     * @param Adapter $adapter
+     */
+    public function __construct(Adapter $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
+    /**
+     * Sends a request and returns it's body
+     *
+     * @param Request $request
+     *
+     * @return string
+     */
+    public function send(Request $request)
+    {
+        $response = $this->adapter->send($request);
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * Creates a request
+     *
+     * @param string      $method
+     * @param string|null $url
+     * @param array       $options
+     *
+     * @return Request
+     */
+    public function createRequest($method, $url = null, array $options = [])
+    {
+        $request = new Message\Request;
+        $protocol_version = '1.1';
+        $body = null;
+        $headers = [];
+
+        extract($options);
+
+        $request->setMethod($method);
+        $request->setUrl($url);
+        $request->setProtocolVersion($protocol_version);
+
+        if (isset($body)) {
+            $body = Stream::create($body);
+            $request->setBody($body);
+        }
+
+        foreach ($headers as $key => $value) {
+            $request->setHeader($key, $value);
+        }
+
+        return $request;
+    }
+
+    /**
+     * Creates and executes a GET request
+     *
+     * @param string|object $url
+     * @param array $options
+     *
+     * @return string
+     */
+    public function get($url = null, array $options = [])
+    {
+        return $this->send($this->createRequest('GET', $url, $options));
+    }
+
+    /**
+     * Creates and executes a POST request
+     *
+     * @param string|object $url
+     * @param array $options
+     *
+     * @return string
+     */
+    public function post($url = null, array $options = [])
+    {
+        return $this->send($this->createRequest('POST', $url, $options));
+    }
+
+    /**
+     * Creates and executes a PUT request
+     *
+     * @param string|object $url
+     * @param array $options
+     *
+     * @return string
+     */
+    public function put($url = null, array $options = [])
+    {
+        return $this->send($this->createRequest('PUT', $url, $options));
+    }
+
+    /**
+     * Creates and executes a HEAD request
+     *
+     * @param string|object $url
+     * @param array $options
+     *
+     * @return string
+     */
+    public function head($url = null, array $options = [])
+    {
+        return $this->send($this->createRequest('HEAD', $url, $options));
+    }
+
+    /**
+     * Creates and executes a DELETE request
+     *
+     * @param string|object $url
+     * @param array $options
+     *
+     * @return string
+     */
+    public function delete($url = null, array $options = [])
+    {
+        return $this->send($this->createRequest('DELETE', $url, $options));
+    }
+
+    /**
+     * Creates and executes a PATCH request
+     *
+     * @param string|object $url
+     * @param array $options
+     *
+     * @return string
+     */
+    public function patch($url = null, array $options = [])
+    {
+        return $this->send($this->createRequest('PATCH', $url, $options));
+    }
+
+    /**
+     * Creates and executes a OPTIONS request
+     *
+     * @param string|object $url
+     * @param array $options
+     *
+     * @return string
+     */
+    public function options($url = null, array $options = [])
+    {
+        return $this->send($this->createRequest('OPTIONS', $url, $options));
+    }
+}
